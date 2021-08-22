89ca4861e550ea7d7070549600d3f15ed7327407
ehdrms1997 <ehdrms1997@naver.com>
2021-08-22 15:07:12 +0900

    test4

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      test3
+      test4
     </div>
     <router-view/>
   </div>
