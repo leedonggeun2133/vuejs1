cdb48a2826a60fd66b0a87379dbfa7033059164c
ehdrms1997 <ehdrms1997@naver.com>
2021-08-22 16:40:53 +0900

    vue router

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,12 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      test7
+      <router-link to="/xxx">xxx</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #c7ac32;
-}
 </style>
